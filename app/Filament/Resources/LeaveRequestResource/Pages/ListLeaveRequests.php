acbc374ce1519b7c3e01419f021597cda7f8fa79
hirusha randunu <hirusharandunu11@gmail.com>
2025-07-25 14:37:20 +1000

    leave request

--- app/Filament/Resources/LeaveRequestResource/Pages/ListLeaveRequests.php
+++ app/Filament/Resources/LeaveRequestResource/Pages/ListLeaveRequests.php
@@ -9,4 +9,14 @@ use Filament\Resources\Pages\ListRecords;
 class ListLeaveRequests extends ListRecords
 {
     protected static string $resource = LeaveRequestResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('🎯 Submit New Leave Request')
+                ->icon('heroicon-o-plus-circle')
+                ->color('success'),
+        ];
+    }
 }

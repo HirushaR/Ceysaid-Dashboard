b6b1b9382cf7725efa541899930b23165c8be8c8
hirusha randunu <hirusharandunu11@gmail.com>
2025-08-05 11:52:43 +1000

    remove deployment

--- .platform/hooks/prebuild/02_install_composer_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/app/staging
-
-# Install Composer dependencies
-/usr/bin/composer.phar install --no-dev --optimize-autoloader --no-interaction --ignore-platform-reqs
-
-echo "Composer dependencies installed successfully"
